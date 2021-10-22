1ca79200dd423b61c882e5cdbab180bece9ee749
myl7 <myl@myl.moe>
2021-10-22 11:26:59 +0800

    feat: Add c bindings

---
+++ src/c.rs
@@ -0,0 +1,36 @@
+#![allow(non_camel_case_types)]
+
+use crate::parse::parse;
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_int};
+
+#[repr(C)]
+pub enum BsizeUnit {
+    kBit = 1,
+    kByte = 2,
+    kNone = 3,
+}
+
+#[repr(C)]
+pub struct BsizeRes {
+    error: c_int,
+    num: u64,
+    unit: BsizeUnit,
+}
+
+#[no_mangle]
+pub extern "C" fn BsizeParse(p: *const c_char, ignore_bi: bool) -> BsizeRes {
+    let s = unsafe { CStr::from_ptr(p) }.to_str().unwrap();
+    match parse(s, ignore_bi) {
+        Ok(d) => BsizeRes {
+            error: 0,
+            num: d.0,
+            unit: d.1.into(),
+        },
+        Err(e) => BsizeRes {
+            error: e.into(),
+            num: 0,
+            unit: BsizeUnit::kNone,
+        },
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 #![feature(try_blocks)]
 
+mod c;
 mod grammar;
 mod parse;
 mod scale;
 
-fn main() {
-    println!("Hello, world!");
-}
+pub use c::{BsizeParse, BsizeRes, BsizeUnit};
+pub use parse::{parse, Error, Unit};
